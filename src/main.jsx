f7d62dfb4055b0668df6828b94a3ef8dbd4c496d
HRE-ECR <Jason.Edwards@hitachirail.com>
2026-02-10 23:11:44 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* IMPORTANT: makes routes live under /ECR_Overtime/ on GitHub Pages */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
